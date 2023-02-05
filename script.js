706c5bf0662135cef27a29bc6674dde34416b8cb
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-05 16:24:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 const app = require('./index.js');
+const input = document.querySelector("input");
+const timerDisplay = document.querySelector("#timer");
+const defaultTimers = document.querySelectorAll(".default-timer");
+
+let countdown;
+
+function startTimer(duration, display) {
+  let timer = duration, minutes, seconds;
+  countdown = setInterval(function() {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      clearInterval(countdown);
+      display.textContent = "Time's up!";
+    }
+  }, 1000);
+}
+
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    clearInterval(countdown);
+    let minutes = parseInt(input.value) || 0;
+    let endTime = new Date(new Date().getTime() + minutes * 60 * 1000);
+    timerDisplay.textContent = "Ends at: " + endTime.toLocaleTimeString();
+    startTimer(minutes * 60, timerDisplay);
+  }
+});
+
+defaultTimers.forEach(function(defaultTimer) {
+  defaultTimer.addEventListener("click", function() {
+    clearInterval(countdown);
+    let minutes = parseInt(defaultTimer.dataset.time) || 0;
+    let endTime = new Date(new Date().getTime() + minutes * 60 * 1000);
+    timerDisplay.textContent = "Ends at: " + endTime.toLocaleTimeString();
+    startTimer(minutes * 60, timerDisplay);
+  });
+});
 
 app.listen(3000, () => {
   console.log('server started');
